d2c897b13e9fc98c56634615b77f3a3fe38d9d37
Chaiprasit Khamsi <chaiprasit.kh.61@ubu.ac.th>
2020-09-02 12:05:08 +0700

    Updates

---
+++ precache-manifest.1bd6d9bc9d99467c6f8eca0a01aab137.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0d5f5d961618a7429c4ea3190084ba3",
+    "url": "/tic-tac-toe1/index.html"
+  },
+  {
+    "revision": "bfd3ac42b49122c95a9b",
+    "url": "/tic-tac-toe1/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/tic-tac-toe1/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe1/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfd3ac42b49122c95a9b",
+    "url": "/tic-tac-toe1/static/js/main.d19970cd.chunk.js"
+  },
+  {
+    "revision": "d21686a44309ec001b48",
+    "url": "/tic-tac-toe1/static/js/runtime-main.d6466a8f.js"
+  }
+]);
